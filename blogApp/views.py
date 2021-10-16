501d9e311aaf0f80fe8459dd36fc66c1021f77c0
Tomasz Czuba <t.czuba95@gmail.com>
2021-10-16 19:37:04 +0200

    Stemming and ranking of results

--- blogApp/views.py
+++ blogApp/views.py
@@ -6,7 +6,7 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 # Create your views here.
@@ -103,9 +103,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
     return render(request, 'blogApp/post/search.html', {'form': form,
                                                         'query': query,
                                                         'results': results})
